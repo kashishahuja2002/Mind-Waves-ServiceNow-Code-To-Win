1857846ec84951cfd40a173806fe02b812f73c54
kashishahuja2002 <kashishahuja2002@gmail.com>
2023-02-18 22:05:02 +0530

    feat/browser-routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import PagesContainer from "./components/pages/PagesContainer";
+
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={"website"}></Route>
+
+        <Route path="/auth/" element={"auth"}>
+          <Route path="signup" element={"signup"} />
+          <Route path="login" element={"login"} />
+        </Route>
+
+        <Route path="/pages/" element={<PagesContainer />}>
+          <Route path="dashboard" element={"dashboard"} />
+          <Route path="eap" element={"eap"} />
+          <Route path="reminders-breaks" element={"reminders-breaks"} />
+          <Route path="daily-activities" element={"daily-activities"} />
+          <Route path="educational-resources" element={"educational-resources"} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
