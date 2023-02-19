d900a4ccd6282f21564fca2955a50f3165bcbc4f
kashishahuja2002 <kashishahuja2002@gmail.com>
2023-02-20 02:01:02 +0530

    feat/daily-stats

---
+++ src/components/pages/dashboard/DailyStats.js
@@ -0,0 +1,41 @@
+import React from "react";
+
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import Typography from '@mui/material/Typography';
+import DashboardIcon from '@mui/icons-material/Dashboard';  // Dashboard Icon
+
+const StatCard = () => {
+  return (
+    <Card variant="outlined" className="daily-stat-card ">
+        <Box className="icon-box"><DashboardIcon /></Box>
+        <Box>
+          <span> Steps count </span>
+          <Typography variant="h4" gutterBottom> 2,300 </Typography>
+        </Box>
+    </Card>
+  );
+}
+
+const DailyStats = () => {
+  return (
+    <Grid
+      container
+      direction="row"
+      justifyContent="space-between"
+      alignItems="centre"
+      className="daily-stats"
+    >
+      <Grid item xs> {StatCard()} </Grid>
+      <Grid item xs> {StatCard()} </Grid>
+      <Grid item xs> {StatCard()} </Grid>
+      <Grid item xs> {StatCard()} </Grid>
+      <Grid item xs> {StatCard()} </Grid>
+      <Grid item xs> {StatCard()} </Grid>
+      <Grid item xs> {StatCard()} </Grid>
+    </Grid>
+  );
+}
+
+export default DailyStats;
--- src/components/pages/dashboard/Dashboard.js
+++ src/components/pages/dashboard/Dashboard.js
@@ -1,8 +1,11 @@
 import React from "react";
 
 import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
-import DailyStats from "./daily-stats/DailyStats";
+import DailyStats from "./DailyStats";
+
+import '../../../styles/pages/dashboard/dashboard.scss';
 
 const Dashboard = () => {
     return (
@@ -13,12 +16,13 @@ const Dashboard = () => {
             alignItems="flex-start"
             className="dashboard"
         >
-            <Grid item sx={{ width: "100%" }}>
+            <Grid item sx={{ mb: 3 }}>
+                <Typography variant="body" gutterBottom className="stat-title">Daily Stats</Typography>
                 <DailyStats />
             </Grid>
 
             <Grid item>
-                Weekly Stats
+                <Typography variant="body" gutterBottom className="stat-title">Weekly Stats</Typography>
             </Grid>
         </Grid>
     );
