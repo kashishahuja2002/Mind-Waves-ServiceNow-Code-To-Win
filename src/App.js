3f6cb07cc48b7c52066398eb03026d75cb705c89
kashishahuja2002 <kashishahuja2002@gmail.com>
2023-02-19 23:45:11 +0530

    Added app className

--- src/App.js
+++ src/App.js
@@ -7,24 +7,26 @@ import './App.scss';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={"website"}></Route>
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={"website"}></Route>
 
-        <Route path="/auth/" element={"auth"}>
-          <Route path="signup" element={"signup"} />
-          <Route path="login" element={"login"} />
-        </Route>
+          <Route path="/auth/" element={"auth"}>
+            <Route path="signup" element={"signup"} />
+            <Route path="login" element={"login"} />
+          </Route>
 
-        <Route path="/pages/" element={<PagesContainer />}>
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="eap" element={"eap"} />
-          <Route path="reminders-breaks" element={"reminders-breaks"} />
-          <Route path="daily-activities" element={"daily-activities"} />
-          <Route path="educational-resources" element={"educational-resources"} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+          <Route path="/pages/" element={<PagesContainer />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="eap" element={"eap"} />
+            <Route path="reminders-breaks" element={"reminders-breaks"} />
+            <Route path="daily-activities" element={"daily-activities"} />
+            <Route path="educational-resources" element={"educational-resources"} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
