a9c881a150f30ede92b1d1b3015215720bb13777
kashishahuja2002 <kashishahuja2002@gmail.com>
2023-02-22 02:13:15 +0530

    hotfix/removed esLint errors

--- src/components/pages/Sidebar.js
+++ src/components/pages/Sidebar.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 
 import Drawer from '@mui/material/Drawer';
