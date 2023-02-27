b9b5bc23ed31980be49e0198170737f768713611
kashishahuja2002 <kashishahuja2002@gmail.com>
2023-02-28 01:00:35 +0530

    feat/bar-loader

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useSelector } from 'react-redux';
+
+import LinearProgress from '@mui/material/LinearProgress';
 
 import AuthContainer from "./components/auth/AuthContainer";
 import PagesContainer from "./components/pages/PagesContainer";
@@ -12,27 +15,33 @@ import Achivements from "./components/pages/achivements/Achivements";
 import './App.scss';
 
 function App() {
+  const loading = useSelector((store) => store.app.barLoading);
+
   return (
-    <div className="app">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={"website"}></Route>
+    <Suspense fallback={<div>Please wait while loading...</div>}>
+      {loading ? <LinearProgress /> : ""}
 
-          <Route path="/auth/login" element={<AuthContainer />}></Route>
+      <div className="app">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={"website"}></Route>
 
-          <Route path="/pages/" element={<PagesContainer />}>
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="eap" element={<Eap />} />
-            <Route path="set-reminders" element={"set-reminders"} />
-            <Route path="daily-activities" element={"daily-activities"} />
-            <Route path="educational-resources" element={<EducationalResources />} />
-            <Route path="profile" element={<Profile />} />
-            <Route path="achivements" element={<Achivements />} />
-            <Route path="leaderboard" element={"Leaderboard"} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
+            <Route path="/auth/login" element={<AuthContainer />}></Route>
+
+            <Route path="/pages/" element={<PagesContainer />}>
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="eap" element={<Eap />} />
+              <Route path="set-reminders" element={"set-reminders"} />
+              <Route path="daily-activities" element={"daily-activities"} />
+              <Route path="educational-resources" element={<EducationalResources />} />
+              <Route path="profile" element={<Profile />} />
+              <Route path="achivements" element={<Achivements />} />
+              <Route path="leaderboard" element={"Leaderboard"} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </Suspense>
   );
 }
 
