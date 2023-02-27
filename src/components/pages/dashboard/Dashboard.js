0a4d012b32f42a2f1dfae81773249739fa1cf0ec
kashishahuja2002 <kashishahuja2002@gmail.com>
2023-02-28 01:02:17 +0530

    feat/google fit APIs on dashboard

--- src/components/pages/dashboard/Dashboard.js
+++ src/components/pages/dashboard/Dashboard.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
 
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
@@ -7,8 +8,88 @@ import DailyStats from "./DailyStats";
 import WeeklyStats from "./WeeklyStats";
 
 import '../../../styles/pages/Dashboard.scss';
+import { getGoogleFitData } from "../../../redux/dashboard/DashboardAction";
+import { updateBarLoading } from "../../../redux/Actions";
 
 const Dashboard = () => {
+
+    const dispatch = useDispatch();
+
+    function getPreviousMonday()
+    {
+        var date = new Date();
+        var day = date.getDay();
+        var prevMonday = new Date();
+        if(date.getDay() == 0){
+            prevMonday.setDate(date.getDate() - 7);
+        }
+        else{
+            prevMonday.setDate(date.getDate() - (day-1));
+        }
+
+        return prevMonday;
+    }
+
+    const formatDate = (value) => {
+        var date = new Date(value);
+
+        date = ((date.getMonth() > 8) ? (date.getMonth() + 1) : ('0' + (date.getMonth() + 1))) + '/' + ((date.getDate() > 9) ? date.getDate() : ('0' + date.getDate())) + '/' + date.getFullYear();
+
+        return date;
+    }
+
+    const getMilliSecond = (date) => {
+        return (new Date(`${date} 00:00:00`).getTime());
+    }
+
+    const getStartEndTime = () => {
+        const startDate = formatDate(getPreviousMonday());
+
+        var endDate = new Date(startDate);
+        endDate.setDate(endDate.getDate() + 7);
+        endDate = formatDate(endDate);
+
+        const startTime = getMilliSecond(startDate);
+        const endTime = getMilliSecond(endDate);
+
+        return {startTime, endTime};
+    }
+
+    useEffect(() => {
+        dispatch(updateBarLoading(true));
+        const time = getStartEndTime();
+
+        const stepCountBody = {
+            "aggregateBy": [{
+                "dataSourceId": "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps"
+            }],
+            "bucketByTime": { "durationMillis": 86400000 },
+            "startTimeMillis": time.startTime,		
+            "endTimeMillis": time.endTime
+        }
+        dispatch(getGoogleFitData(stepCountBody, "STEP_COUNT"));
+
+        const heartPointsBody = {
+            "aggregateBy": [{
+                "dataSourceId": "derived:com.google.heart_minutes:com.google.android.gms:merge_heart_minutes"
+            }],
+            "bucketByTime": { "durationMillis": 86400000 },
+            "startTimeMillis": time.startTime,		
+            "endTimeMillis": time.endTime
+        }
+        dispatch(getGoogleFitData(heartPointsBody, "HEART_POINTS"));
+
+        const caloriesBurnedBody = {
+            "aggregateBy": [{
+                "dataSourceId": "derived:com.google.calories.expended:com.google.android.gms:merge_calories_expended"
+            }],
+            "bucketByTime": { "durationMillis": 86400000 },
+            "startTimeMillis": time.startTime,		
+            "endTimeMillis": time.endTime
+        }
+        dispatch(getGoogleFitData(caloriesBurnedBody, "CALORIES_BURNED"));
+    }, [])
+
     return (
         <Grid
             container
