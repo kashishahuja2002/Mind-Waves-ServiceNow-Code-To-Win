b1dcd32a66584773b78bf2c5cb4536861daf791d
kashishahuja2002 <kashishahuja2002@gmail.com>
2023-03-07 02:31:25 +0530

    feat/add mood api

--- src/redux/daily-activities/DailyActivitiesActionTypes.js
+++
@@ -1,5 +0,0 @@
-const ADD_WATER = 'ADD_WATER';
-
-export default {
-    ADD_WATER,
-}
--- src/redux/questionnaire/QuestionnaireActions.js
+++ src/redux/questionnaire/QuestionnaireActions.js
@@ -1,11 +1,11 @@
 import http from '../../services/_httpServices';
 
-export const addWater = (url, body) => {
+export const addMood = (url, body) => {
     return (dispatch) => {
         http.HttpCall(url, "post", {}, body)
             .then((response) => {
                 if(response.data.status === 200) {
-                    console.log("Successfully added water: ", response.data.data);
+                    console.log("Successfully added Mood: ", response.data.data);
                 }
             })
             .catch((error) => {
