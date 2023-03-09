473bd5ace189ab0b6424d78df6f9f56c6f441f69
kashishahuja2002 <kashishahuja2002@gmail.com>
2023-03-09 22:46:00 +0530

    feat/build-v4

--- backend/server/bin/www.js
+++ backend/server/bin/www.js
@@ -30,7 +30,7 @@ mongoose.connect(uri, {
 
 app.use(express.static(path.join(__dirname, '../../../build')));
 
-app.get('/*', function (req, res) {
+app.get('https://www.mindwaves.live/', function (req, res) {
   res.sendFile(path.join(__dirname, '../../../build', 'index.html'));
 });
 
