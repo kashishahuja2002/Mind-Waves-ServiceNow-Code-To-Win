b77d7eebfabe5e82564263a8ea625a756ff344db
kashishahuja2002 <kashishahuja2002@gmail.com>
2023-12-17 02:18:05 +0530

    fix/base-url

--- src/services/_httpServices.js
+++ src/services/_httpServices.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// const BaseUrl = "http://localhost:8000/";
-const BaseUrl = "https://www.mindwaves.live/";
+const BaseUrl = "http://localhost:8000/";
+// const BaseUrl = "https://www.mindwaves.live/";
 
 // For Post Api Calls And Put
 const HttpCall = async (url, method, params, body) => {
